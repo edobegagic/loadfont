bbb6f8f105c6a569865adae4597a7ffd076649fd
nicma777 <manuel@tempusmedia.hr>
2020-02-11 10:22:56 +0100

    loadFont
    loadFont-example
    https://dev.to/edo_begagic/add-custom-fonts-to-react-native-expo-app-54d

---
+++ App.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    sriracha: require('./assets/fonts/Sriracha-Regular.ttf')
+  });
+};
+
+export default function App() {
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  if (!dataLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setDataLoaded(true)}
+      />
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={{ fontFamily: 'sriracha' }}>
+        Open up App.js to start working on your app!
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
